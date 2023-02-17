4ccadfa23087e46e8ca5aaa408554021b3aeb911
Ingonyam <pmfhisowengonyama@gmail.com>
2023-02-16 18:49:44 -0800

    programming in c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	printf("size of char is: %i byte\n",sizeof(char));
-	printf("size of int is: %i bytes\n",sizeof(int));
-	printf("size of long int is: %i bytes\n",sizeof(long int));
-	printf("size of long long int is: %i bytes\n",sizeof(long long int));
-	printf("size of float is: %i bytes\n",sizeof(float));
+	printf("size of char is: %i byte\n", sizeof(char));
+	printf("size of int is: %i bytes\n", sizeof(int));
+	printf("size of long int is: %i bytes\n", sizeof(long int));
+	printf("size of long long int is: %i bytes\n", sizeof(long long int));
+	printf("size of float is: %i bytes\n", sizeof(float));
 	return (0);
-}	
+}
