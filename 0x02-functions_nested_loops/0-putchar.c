44c0f3eff99cb56dcb6c0e5c52a3a74a7d71dd5f
Ingonyam <pmfhisowengonyama@gmail.com>
2023-02-21 14:43:36 -0800

    functions nested loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
 
 int main(void)
-
 {
-	printf("_putchar\n");
+	printf(;"_putchar\n");
 	return (0);
 }
