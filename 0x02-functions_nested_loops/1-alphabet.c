62aa779bcdbdfc02b02f971154ab56e26e5cf05d
Ingonyam <pmfhisowengonyama@gmail.com>
2023-02-22 16:14:48 -0800

    functions nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/*
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void);
+
+int main(void)
+{	
+	print_alphabet();
+	return 0;
+}
+
+void print_alphabet(void)
+{
+
+	char(alphabet);
+
+
+	for(alphabet = 'a' ; alphabet <= 'z' ; alphabet++){
+		
+	putchar(alphabet);
+	
+	putchar('\n');}
+}
